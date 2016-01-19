f672d1bade54c533aeb7b739b1ac9248c92f92e2
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2016-01-19 12:50:17 -0800

    Phase 2 started

--- p1/shell.c
+++ p1/shell.c
@@ -24,58 +24,51 @@ int main(int argc, char **argv) {
 	
 	char str[129];
 	
-	printf(">");
-	fgets(str,129,stdin);
-	
-	char** args = args_from_str(str);
-	int i = 0;
-	
-	while(args[i]!=NULL) {
-		printf("%s\n", args[i]);
-		i++;
-	}
-	
-	return 0;
-	
-	pid_t pid = fork(); //create a new process
-	
-	//CHILD
-	if (pid == 0) { 
-		//prepare arguments for exec function
-		char** cmd_args = args_from_cmdline(argc, argv);
+	while(1) {
+		printf(">");
+		fgets(str,129,stdin);
+		//TODO: check string size and pop up error if it's invalid or continue if it's zero
+		char** cmd_args = args_from_str(str);
 		
-		if(execvp(cmd_args[0], cmd_args) == -1){ //error with de command
-			perror(NULL);//print error message
+		if(strcmp(cmd_args[0],"exit") == 0) break;
+		
+		pid_t pid = fork(); //create a new process
+		
+		//CHILD
+		if (pid == 0) { 
+			
+			if(execvp(cmd_args[0], cmd_args) == -1){ //error with de command
+				perror(NULL);//print error message
+				free_args(cmd_args);//free memory allocaded in args_from_cmdline function
+				exit(EXIT_FAILURE);
+			}
 			free_args(cmd_args);//free memory allocaded in args_from_cmdline function
+
+		}else if (pid > 0) { //PARENT
+			struct timeval init, end; //checkpoint to measure wall-clock time
+			int status = 0;//return from wait function
+			
+			gettimeofday(&init,NULL);
+			wait(&status);//wait for child execution
+			gettimeofday(&end,NULL);
+			
+			//if the child returned EXIT_FAILURE (when something goes wrong)
+			if(WEXITSTATUS(status) == EXIT_FAILURE)
+				exit(EXIT_FAILURE);
+			
+			//get child execution statistics
+			struct rusage rep;
+			getrusage(RUSAGE_CHILDREN,&rep);
+			
+			//Print report
+			printf("\n***REPORT***\n");
+			printf("wall-clock:\t\t%d\n",diff_time(init,end));
+			print_rusage(rep);
+		
+		}else { //error
+			perror(NULL);//error in fork
 			exit(EXIT_FAILURE);
 		}
-		free_args(cmd_args);//free memory allocaded in args_from_cmdline function
-
-	}else if (pid > 0) { //PARENT
-		struct timeval init, end; //checkpoint to measure wall-clock time
-		int status = 0;//return from wait function
-		
-		gettimeofday(&init,NULL);
-		wait(&status);//wait for child execution
-		gettimeofday(&end,NULL);
-		
-		//if the child returned EXIT_FAILURE (when something goes wrong)
-		if(WEXITSTATUS(status) == EXIT_FAILURE)
-			exit(EXIT_FAILURE);
-		
-		//get child execution statistics
-		struct rusage rep;
-		getrusage(RUSAGE_CHILDREN,&rep);
-		
-		//Print report
-		printf("\n***REPORT***\n");
-		printf("wall-clock:\t\t%d\n",diff_time(init,end));
-		print_rusage(rep);
-	
-	}else { //error
-		perror(NULL);//error in fork
-		exit(EXIT_FAILURE);
 	}
-	
 	return 0;
 }
