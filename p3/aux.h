e729877df57766cacfda3992a1747bf360887d80
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2016-02-10 21:39:02 -0800

    Added order_sem_t. Not tested yet

--- p3/aux.h
+++ p3/aux.h
@@ -3,5 +3,30 @@
 
 int uniform_rand(int low, int high);
 
+#include "queue.h"
+#include <pthread.h>
+#include <semaphore.h>
+
+typedef struct {
+	sem_t main_sem;
+	sem_t queue_sem;
+	
+	unsigned int first;
+	unsigned int last;
+	unsigned int capacity;
+	unsigned int size;
+	
+	sem_t *queue;
+} order_sem;
+
+int order_sem_init(order_sem *sem, unsigned int value, unsigned int max_threads);
+
+int order_sem_wait(order_sem *sem);
+
+int order_sem_post(order_sem *sem);
+
+int order_sem_destroy(order_sem *sem);
+
+int order_sem_trywait(order_sem *sem);
 
 #endif
