b2a75399c9315d14c71b0fac872cc62bd9f6d24b
Mateus Amarante Araujo <mateus.amarujo@gmail.com>
2016-02-22 23:01:37 -0800

    Implemented create_table with find_evict_first technique

--- p4/memory.c
+++ p4/memory.c
@@ -1,10 +1,13 @@
 #include <stdint.h>
 
 #include "memory.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
 
-#define RAM_SIZE 25
-#define SSD_SIZE 100
-#define HDD_SIZE 1000
+#define RAM_SIZE 3
+#define SSD_SIZE 6
+#define HDD_SIZE 15
 
 #define PAGE_TABLE_SIZE HDD_SIZE
 
@@ -19,6 +22,12 @@ uint32_t hdd_m[HDD_SIZE];
 #define SSD_BIT 1
 #define HDD_BIT 2
 
+//Access time (in seconds)
+#define TIME_FACTOR 100
+#define RAM_TIME .01/TIME_FACTOR
+#define SSD_TIME .1/TIME_FACTOR
+#define HDD_TIME 2.5/TIME_FACTOR
+
 typedef struct {
 	uint8_t flags;//Modified-Referenced
 	uint8_t where;//R-S-H bits
@@ -32,19 +41,26 @@ uint32_t ssd_bitmap[4];//128 bits (use the first 100)
 //we don't need for hhd
 
 
+
+
+int ptable_cursor = 0;
+int  ram_cursor = 0;
+int  ssd_cursor = 0;
+
 //GENERIC BIT OPERATIONS
 //bitmap can be a sigle integer or an array
-int find_empty(uint32_t* bitmap, int from, int size){
+int find_empty(uint32_t* bitmap, int* cursor, int size){
 	
-	int i = from;
+	int i = *cursor;
 	
 	do {
 		if( !(bitmap[i/size] & (1 << i%size)) ){
 			//manage page table and allocate memory
+			*cursor = (i+1)%size;
 			return i;
 		}
 		i = (i+1)%size;
-	}while(i!=from);
+	}while(i != *cursor);
 	
 	return -1;
 }
@@ -55,24 +71,120 @@ void set_bit(uint32_t* number, int pos, int bit_value, int size) {
 }
 
 int get_bit(uint32_t* number, int pos, int size){
-	return number[pos/size] & (1 << pos%size);
+	return (number[pos/size] & (1 << pos%size))&&1;
+}
+
+void print_bitmap(uint32_t* bitmap, int size) {
+	int i;
+	for (i = 0; i < size; i++)
+		printf("%d", get_bit(bitmap, i, size));
+}
+
+void print_page_entry(page_entry_t entry) {
+	print_bitmap((uint32_t*)&entry.flags, 2);
+	printf(" | ");
+	print_bitmap((uint32_t*)&entry.where, 3);
+	printf(" | %d | %d\n", entry.ram_addr, entry.ssd_addr);
 }
 
 page_entry_t page_table[PAGE_TABLE_SIZE];
 
-vAddr create_page() {
-	static int i = -1;
-	i = (i+1)%PAGE_TABLE_SIZE;
+//Auxiliar function (return a random number between low and high)
+int uniform_rand(int low, int high) {
+	return rand()%(abs(high-low)+1)+low;
+}
+
+int find_evict_first(int mem_bit, int mem_size) {
+	int i = ptable_cursor;//start point
 	
-	int end = (i+PAGE_TABLE_SIZE-1)%PAGE_TABLE_SIZE;
-	
-	while(i!=end){
-		if(!page_table[i].where){
-			//manage page table and allocate memory
+	do {
+		if( get_bit((uint32_t*) &page_table[i].where, mem_bit, 3) ){
+			//ptable_cursor = (i+1)%PAGE_TABLE_SIZE;
 			return i;
 		}
 		i = (i+1)%PAGE_TABLE_SIZE;
+	}while(i!=ptable_cursor);
+	
+	return -1;//not supposed to reach this position
+}
+
+
+int evict() {
+	int ram_addr, ssd_addr;
+	
+	//Virtual address of the process to be evicted from ram
+	ram_addr = find_evict_first(RAM_BIT, RAM_SIZE); 
+	
+	printf("evicted %d from ram\n", ram_addr);
+	
+	//vAddr in ssd. Potencially can be evicted from ssd
+	ssd_addr = find_empty(&ssd_bitmap[0], &ssd_cursor, SSD_SIZE);
+	
+	if(ssd_addr < 0) {//full
+		ssd_addr = find_evict_first(SSD_BIT, SSD_SIZE); 
+		printf("evicted %d from ssd\n", ssd_addr);
+		//Copy from SSD to HDD
+		
+		//set bits in page table
+		set_bit((uint32_t*) &page_table[ssd_addr].where, SSD_BIT, 0, 3);//take out from ssd
+		set_bit((uint32_t*) &page_table[ssd_addr].where, HDD_BIT, 1, 3);//put into hdd
+		
+		//access time
+		usleep(HDD_TIME*1e6 + SSD_TIME*1e6);
+		
+		//copy the values
+		hdd_m[ssd_addr] = ssd_m[page_table[ssd_addr].ssd_addr];
+	}else {
+		set_bit(&ssd_bitmap[0], ssd_addr, 1, SSD_SIZE);
 	}
+	print_bitmap(&ssd_bitmap[0], SSD_SIZE);
+	printf("\n");
+	//Copy from RAM to SSD
+	
+	//set bits in page table
+	set_bit((uint32_t*) &page_table[ram_addr].where, RAM_BIT, 0, 3);
+	set_bit((uint32_t*) &page_table[ram_addr].where, SSD_BIT, 1, 3);
+	
+	//access time
+	usleep(RAM_TIME*1e6 + SSD_TIME*1e6);
+	
+	//copy the values
+	ssd_m[page_table[ram_addr].ssd_addr] = ram_m[page_table[ram_addr].ram_addr];
+	page_table[ram_addr].ssd_addr = ssd_addr;
+
+	return page_table[ram_addr].ram_addr;//return evicted vAddr
+}
+
+vAddr create_page() {
+	int i = ptable_cursor;
+	
+	do{
+		if(!page_table[i].where){//not used page-table
+			//update page table
+			ptable_cursor = (i+1)%PAGE_TABLE_SIZE;
+			
+			set_bit((uint32_t*) &page_table[i].where, RAM_BIT, 1, 3);			
+			
+			int ram_addr = find_empty(&ram_bitmap, &ram_cursor, RAM_SIZE);
+			
+			if (ram_addr < 0)
+				ram_addr = evict();//page fault
+			else
+				set_bit(&ram_bitmap, ram_addr, 1, RAM_SIZE); 
+				
+			page_table[i].ram_addr = ram_addr;
+			
+			int j;
+			
+			for (j = 0; j < PAGE_TABLE_SIZE; j++)
+				print_page_entry(page_table[j]);
+			printf("------------\n");
+			
+			return i;
+		}
+		i = (i+1)%PAGE_TABLE_SIZE;
+	
+	}while(i != ptable_cursor);
 	
 	return -1;//memory is full
 }
--- p4/test.c
+++ p4/test.c
@@ -22,6 +22,11 @@ void memoryMaxer() {
 }
 
 int main() {
-
+	
+	int i;
+	
+	for (i = 0; i < 15; i++)
+		create_page();
+	
 	return 0;
 }
